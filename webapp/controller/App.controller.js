2faa9649c365047c905d4a65b7d3771837b4c3de
vikas.patharitech <vikas.mishra@patharitech.com>
2021-10-25 15:56:04 +0530

    Step 5: Controllers

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.App",  {
+        onShowHello: function () {
+            alert("Hello there alert!")
+        }
+    });
+})
