75a6e62d40221ade19b4d6bf22b712aba4037e95
vikas.patharitech <vikas.mishra@patharitech.com>
2021-10-25 23:03:37 +0530

    Step 10: Descriptor for Applications

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,13 +9,8 @@ sap.ui.define([
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 
         metadata: {
-            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-                "id": "app"
-            }
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
         },
 
         init: function () {
