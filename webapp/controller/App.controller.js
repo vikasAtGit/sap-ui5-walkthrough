88ad845f54530351cea229a059ea2b92448faf66
vikas.patharitech <vikas.mishra@patharitech.com>
2021-10-25 22:57:11 +0530

    Step 9: Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,33 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/m/MessageToast"
 
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App",  {
-
-        onInit: function() {
-            //set the model on the view
-
-            var oData = {
-                recipient: {
-                    name: "UI5"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-
-            //set i18n model on view
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.getView().setModel(i18nModel, "i18n")
-        },
 
         onShowHello: function () {
             //read a msg from i18n model
